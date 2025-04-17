71f42a66102c5b63976fbb7b4d1b0bdff5c2c71a
DoesA50Hit <mm0rp9p1ay3r@gmail.com>
2025-04-17 02:13:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import random
 import streamlit as st
 
+st.set_page_config(page_title="Power Generator", layout="centered")
+st.title("\U0001F300 Power Generator")
+
+st.markdown("Welcome to the Power Generator! Click the button below to generate random power details.")
+
