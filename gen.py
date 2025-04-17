d7622b3ac515ee0817c954f7e202e8e500550409
DoesA50Hit <mm0rp9p1ay3r@gmail.com>
2025-04-17 02:49:14 -0600

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,7 +1,10 @@
 import random
+from lib import CLASSES, WORDS
 
 def generate_random_class():
   """
   Pick a random class and return it
   """
-  
+  char_class = random.choice(list(CLASSES.keys()))
+  return char_class
+
